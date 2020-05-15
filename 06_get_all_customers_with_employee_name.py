62e3fe38590b44133c6dc6c7f7d078e7ee6aed8d
Arif <arif.cse.baust@gmail.com>
2020-05-15 22:09:08 +0600

    get all customers with employee name

---
+++ 06_get_all_customers_with_employee_name.py
@@ -0,0 +1,34 @@
+'''
+This is a template code that you will use in this exercise.
+You only can change the value of query.
+
+'''
+
+# import sqlite
+import sqlite3
+
+# create database connection
+db = sqlite3.connect('employee_basic_crud.db')
+cursor = db.cursor()
+
+# write your query here
+query = ''' SELECT
+            c.firstname,e.firstname
+            FROM
+            customers c
+            inner join
+            employees e
+            on c.supportrepId=e.employeeid'''
+
+
+try:
+    cursor.execute(query)
+    all_rows = cursor.fetchall()
+    for row in all_rows:
+        print(row)
+except:
+    print('Something wrong in your query')
+
+# close database connection
+db.close()
+
